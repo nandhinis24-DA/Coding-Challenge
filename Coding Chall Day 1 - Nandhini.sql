b5786aab6fd7ea4bcd5f9fbe0387c24b563ddbc7
nandhinis24-DA <nannandhu24@gmail.com>
2025-10-17 20:55:19 +0530

    Add files via upload

---
+++ Coding Chall Day 1 - Nandhini.sql
@@ -0,0 +1,24 @@
+create database KMCHHospital;
+use KMCHHospital;
+
+create  table patients(
+Patients_ID int primary key,
+Patient_name varchar(100),
+Age int,
+Gender enum("M" , "F"),
+AdmissionDate date
+);
+
+-- Add column
+alter table patients
+add DoctorAssigned varchar(50);
+
+-- Modify column
+alter table patients
+Modify Patient_name varchar(100);
+
+-- Rename - Table
+rename table patients to Patients_Info;
+
+truncate table Patients_Info;
+drop table Patients_Info;
